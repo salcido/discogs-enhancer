806fb2e6cf586c26036c4a99ada77132aa3f77b1
Matthew Salcido <mattsalcido@gmail.com>
2016-05-01 19:23:48 -0700

    Initial work on review notifications

---
+++ js/feedback-notifier.js
@@ -0,0 +1,75 @@
+/**
+ *
+ * Discogs Enhancer
+ *
+ * @author: Matthew Salcido (c) 2016
+ * @url: http://www.msalcido.com
+ * @github: https://github.com/salcido
+ *
+ */
+
+$(document).ready(function() {
+
+  let
+      countBuyer = localStorage.getItem('countBuyer'),
+      countSeller = localStorage.getItem('countSeller'),
+      d = new Date(),
+      language = resourceLibrary.language(),
+      lastChecked = Number(localStorage.getItem('profileCountLastChecked')),
+      tenMins = lastChecked + 600000,
+      timeInMillis = d.getTime(),
+      user = $('#site_account_menu').find('.user_image').attr('alt');
+
+  if (language === 'en') {
+
+    language = '';
+
+  } else {
+
+    language = language + '/';
+  }
+
+  if (!lastChecked || timeInMillis > tenMins) {
+
+    $.ajax({
+
+      url: 'https://www.discogs.com/' + language + 'user/' + user,
+
+      type: 'GET',
+
+      dataType: 'html',
+
+      success: function(response) {
+
+        let
+            buyer = $('a[href*="buyer_feedback"]').text(),
+            seller = $('a[href*="seller_feedback"]').text();
+
+        // inital counts
+        if (!countBuyer) {
+
+          localStorage.setItem('countBuyer', buyer);
+        }
+
+        if (!countSeller) {
+
+          localStorage.setItem('countSeller', seller);
+        }
+
+        // Set timestamp when checked
+        localStorage.setItem('profileCountLastChecked', timeInMillis);
+
+        // TODO add badges to nav
+        if (seller > countSeller) {
+
+          console.log('new seller review');
+        }
+
+        if (buyer > countBuyer) {
+
+          console.log('new buyer review');
+        }
+      }
+    });
+  }
+});
