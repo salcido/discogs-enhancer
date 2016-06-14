be4222360536f0b10c48724c427015780b725594
Matthew Salcido <matthew.salcido@healthsparq.com>
2016-06-14 09:21:37 -0700

    cleaned up about.html

---
+++ js/notes-char-count.js
@@ -0,0 +1,3 @@
+// $('.cw_block_collection .notes_field.notes_edited .notes_show .notes_text').first().text().length
+
+// $('.cw_block_wantlist .notes_field.notes_edited .notes_show .notes_text').first().text().length
