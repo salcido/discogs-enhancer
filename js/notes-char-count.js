d980b455f0cc94d6f56517dfae79356f6a821902
Matthew Salcido <mattsalcido@gmail.com>
2016-08-06 12:43:07 -0700

    Character count notes

--- js/notes-char-count.js
+++ js/notes-char-count.js
@@ -1,3 +1,11 @@
 // $('.cw_block_collection .notes_field.notes_edited .notes_show .notes_text').first().text().length
 
 // $('.cw_block_wantlist .notes_field.notes_edited .notes_show .notes_text').first().text().length
+
+// when editing, this is the char count
+// $('.cw_block_collection .notes_field .notes_edit .notes_textarea').val().length
+
+// length of focus input
+// setInterval(function(){console.log($(':focus').val().length)}, 1000)
+
+// .notes_show trigger?
