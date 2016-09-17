fb897f5fc3366112c4d7bb3271d1f05b9fb0dbae
Matthew Salcido <matthew.salcido@healthsparq.com>
2016-09-16 18:38:33 -0700

    format shortcuts url detection

--- js/format-shortcuts.js
+++ js/format-shortcuts.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
         break;
       }
 
-      if (t[i].className.indexOf('forum_reply') > -1) {
+      if ( t[i].className.indexOf('forum_reply') > -1 ) {
 
         hasReview = true;
 
@@ -33,6 +33,7 @@ $(document).ready(function() {
     let markup = '<div class="quick-menu">' +
                     '<a href="#"><div class="quick-button quick-bold">B</div></a>' +
                     '<a href="#"><div class="quick-button quick-italic">I</div></a>' +
+                    '<a href="#"><div class="quick-button quick-link">&#127760;</div></a>' +
                  '</div>';
 
     $(markup).insertAfter( $('textarea') );
@@ -56,6 +57,50 @@ $(document).ready(function() {
 
       // set the focus
       textarea.focus();
+    });
+
+    // URLs
+    $('.quick-button.quick-link').click(function(event) {
+
+      event.preventDefault();
+
+      let link = window.prompt('Paste your artist, release, or user URL here:');
+
+      switch (true) {
+
+        case link.indexOf('/artist/') > -1:
+            console.log('link is a artist');
+            break;
+
+        case link.indexOf('/guidelines/') > -1:
+            console.log('link is a guideline');
+            break;
+
+        case link.indexOf('/label/') > -1:
+            console.log('link is a label');
+            break;
+
+        case link.indexOf('/master/') > -1:
+            console.log('link is a master');
+            break;
+
+        case link.indexOf('/release/') > -1:
+            console.log('link is a release');
+            break;
+
+        case link.indexOf('/topic/') > -1:
+            console.log('link is a topic');
+            break;
+
+        case link.indexOf('/user/') > -1:
+            console.log('link is a user');
+            break;
+
+        default:
+            console.log('a link has no name');
+            break;
+      }
+
     });
   }
 });
