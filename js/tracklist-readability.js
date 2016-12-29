6a60096f627e458210ff94b29ddeea6cc36e1a62
Matthew Salcido <mattsalcido@gmail.com>
2016-12-29 11:05:06 -0800

    Added other media functionality

--- js/tracklist-readability.js
+++ js/tracklist-readability.js
@@ -8,9 +8,9 @@
  *
  */
 
-// TODO: Similar logic for CD/Digital releases?
 // TODO: Add config option to insert spacer between all sides regardless of numeration
-// TODO: fix https://www.discogs.com/Joey-Negro-Universe-Of-Love/release/68453
+// TODO: create config object
+// TODO: check for multiple formats and default to nth?
 
 // Examples:
 // https://www.discogs.com/STL-Night-Grooves/release/1121308
@@ -26,15 +26,18 @@ $(document).ready(function() {
     let
         // don't insert spacers if headings or index tracks already exist.
         // using 1 or less because of possible inclusion of track durations feature
-        noHeadingsOrIndex = $('.track_heading').length <= 1 && $('.index_track').length === 0,
+        noHeadingsOrIndex = $('.track_heading').length < 1 && $('.index_track').length === 0,
 
         // only insert spacers if there are more than n tracks
         // TODO: value is user definable?
-        listLength = $('.playlist tbody tr').length > 8,
+        playlistLength = $('.playlist tbody tr').length,
 
         // Vinyl, casettes ...
         isMultiSided = $('.profile').html().indexOf('/search/?format_exact=Vinyl') > -1 ||
                        $('.profile').html().indexOf('/search/?format_exact=Cassette') > -1,
+
+        multiSidedThreshold = 8,
+        singleSidedThreshold = 15,
 
         // Compilations have different markup requirements when rendering track headings...
         isCompilation = $('.tracklist_track_artists').length > 0,
@@ -52,7 +55,7 @@ $(document).ready(function() {
         prefs = JSON.parse(localStorage.getItem('readability')),
         display = prefs ? '' : 'style="display:none;"',
         spacer = '<tr class="tracklist_track track_heading de-spacer" ' + display + '><td class="tracklist_track_pos"></td><td colspan="2" class="tracklist_track_title">&nbsp;</td>' + duration + '</tr>',
-        trigger = prefs ? '<a class="smallish fright de-spacer-trigger">Hide Readability</a>' : '<a class="smallish fright de-spacer-trigger">Show Readability</a>';
+        trigger = prefs ? '<a class="smallish fright de-spacer-trigger">Hide Dividers</a>' : '<a class="smallish fright de-spacer-trigger">Show Dividers</a>';
 
     /**
      * Looks at an array and determines if it has
@@ -82,7 +85,8 @@ $(document).ready(function() {
      * index of the array differs from the current index.
      *
      * It's designed to find the differences in sides on a
-     * tracklist like: A1, A2, *insert html here* B1, B2
+     * tracklist like when the numbers are sequential
+     * eg: A1, A2, *insert html here* B3, B4, C5, C6 ...
      *
      * @method insertSpacersUsingAlphabet
      * @param  {array} arr [the array to iterate over]
@@ -102,34 +106,89 @@ $(document).ready(function() {
       });
     }
 
-    // Draxx them sklounst
-    if (noHeadingsOrIndex && listLength && isMultiSided) {
+    /**
+     * Inserts a spacer after every nth track
+     * Used for releases that are CDs, files, VHS, DVD, etc...
+     *
+     * @method   insertSpacersEveryNth
+     * @param    {array}  arr [the array to iterate over]
+     * @param    {number} nth [the number of tracks before a spacer is inserted]
+     * @return   {undefined}
+     */
+    function insertSpacersEveryNth(arr, nth) {
 
-      let trackpos = $('.tracklist_track_pos').map(function() { return $(this).text(); });
+      arr.each((i) => {
 
-      // **********************
-      // UI functionality
-      // **********************
+        if (i % nth === 0 && i !== 0) {
 
-      // Append trigger element
-      $('#tracklist .group').append(trigger);
-
-      // Trigger functionality
-      $('.de-spacer-trigger').on('click', function() {
-
-        if ($('.de-spacer').is(':visible')) {
-
-          $(this).text('Show Readability');
-          localStorage.setItem('readability', false);
-
-        } else {
-
-          $(this).text('Hide Readability');
-          localStorage.setItem('readability', true);
+          $(spacer).insertAfter(tracklist[i - 1]);
         }
-
-        $('.de-spacer').toggle('fast');
       });
+    }
+
+    /**
+     * Inserts a spacer if the next track's number is less than the
+     * current tracks number (eg: A2, B1 ...) or the current track
+     * has no number and the next one does (eg: A, B1, ...)
+     *
+     * @method   insertSpacersBasedOnSides
+     * @param    {array} arr [the array to iterate over]
+     * @return   {undefined}
+     */
+    function insertSpacersBasedOnSides(arr) {
+
+      try {
+
+        arr.each(function(i) {
+
+          let current = Number(arr[i].match(/\d+/g)),
+              next = Number(arr[i + 1].match(/\d+/g));
+
+          // check for 0 value which can be returned when a
+          // track is simply listed as A, B, C, etc ...
+          if ( next <= current && current !== 0 || !current && next ) {
+
+            $(spacer).insertAfter(tracklist[i]);
+          }
+        });
+      } catch (e) {
+        // just catch the errors
+      }
+    }
+
+    // =======================================
+    // UI functionality
+    // =======================================
+
+    // Append trigger element
+    $('#tracklist .group').append(trigger);
+
+    // Trigger functionality
+    $('.de-spacer-trigger').on('click', function() {
+
+      if ($('.de-spacer').is(':visible')) {
+
+        $(this).text('Show Dividers ');
+        localStorage.setItem('readability', false);
+
+      } else {
+
+        $(this).text('Hide Dividers ');
+        localStorage.setItem('readability', true);
+      }
+
+      $('.de-spacer').toggle('fast');
+    });
+
+    // =======================================
+    // DOM manipulation
+    // =======================================
+
+    // Vinyl and cassettes
+    if (noHeadingsOrIndex && playlistLength > multiSidedThreshold && isMultiSided) {
+
+      // Get the track positions from the playlist
+      let trackpos = $('.tracklist_track_pos').map(function() { return $(this).text(); });
 
       // Populate our arrays with whatever the prefix is and the remaining numbers
       trackpos.each(function(i, tpos) {
@@ -144,50 +203,47 @@ $(document).ready(function() {
 
       if (isSequential) {
 
-        // if the numbering is sequential, use the alpha-prefixes to
-        // determin where to insert the spacer markup
+        // if the numbering is sequential (eg: A1, A2, B3, B4, C5, C6, C7 ...),
+        // use the alpha-prefixes to determine where to insert the spacer markup
         return insertSpacersUsingAlphabet(prefix);
 
       } else {
 
-        try {
-
-          trackpos.each(function(i) {
-
-            let current = Number(trackpos[i].match(/\d+/g)),
-                next = Number(trackpos[i + 1].match(/\d+/g));
-
-            // if the next track's number is less than the current tracks number (ie: A2, B1 ...)
-            // or the current track has no number and the next one does (ie: A, B1, ...)
-            // (also check for 0 value which may be returned when a track is simply listed as A, B, C, etc ...)
-            if ( next <= current && current !== 0 || !current && next ) {
-
-              $(spacer).insertAfter(tracklist[i]);
-            }
-          });
-        } catch (e) {
-          // just catch the errors; don't throw them
-        }
+        // If the numbering is not sequential (eg: A1, A2, B, C1, C2)
+        return insertSpacersBasedOnSides(trackpos);
       }
+    }
+
+    // Non-sided releases (Digital, CD, etc...)
+    if (noHeadingsOrIndex && playlistLength > singleSidedThreshold && !isMultiSided) {
+
+      insertSpacersEveryNth(tracklist, 5);
     }
   }
 });
 
+// config object
+// {
+//   vcThreshold: 8,
+//   otherMediaThreshold: 15,
+//   vcReadability: true,
+//   otherMediaReadability: true,
+// }
 
 // Insert spacers between every different side:
 // TODO: make this an option
 /*
 
-  if (noHeadingsOrIndex && listLength && isMultiSided) {
+  if (noHeadingsOrIndex && playlistLength && isMultiSided) {
 
     let tracklist = $('.playlist tbody tr'),
-        // array of strings (ie: ["A1", "A2", "B1", "B2" ...])
+        // array of strings (eg: ["A1", "A2", "B1", "B2" ...])
         trackpos = $('.tracklist_track_pos').map(function() { return $(this).text(); });
 
     trackpos.each(function(i, tpos) {
 
       // If the next track begins with a different letter
-      // (ie: A1, A2, B - where B is the next track), insert the spacer.
+      // (eg: A1, A2, B - where B is the next track), insert the spacer.
       //
       // Check for falsy value with `!tracklist[1].classList.contains('track_heading')`
       // in case tracktimes option gets injected before this has run. Prevents a
