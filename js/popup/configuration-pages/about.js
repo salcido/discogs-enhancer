ab196c5d907472529f9a23734db250e2a517e614
Matthew Salcido <matthew.salcido@healthsparq.com>
2017-06-13 14:57:10 -0700

    Light refactor

--- js/popup/configuration-pages/about.js
+++ js/popup/configuration-pages/about.js
@@ -6,11 +6,19 @@
  * @website: http://www.msalcido.com
  * @github: https://github.com/salcido
  *
+ * ---------------------------------------------------------------------------
+ * Overview
+ * ---------------------------------------------------------------------------
+ *
+ * This will inject the Version and Date strings into the DOM and populate the
+ * quick naviation select box with all `.feature` elements. The script begins
+ * execution at the bottom below the `Init / DOM Setup` comment block.
+ *
  */
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  let select = document.getElementById('nav-select');
+  const select = document.getElementById('nav-select');
 
   // ======================================================
   // Functions
@@ -22,15 +30,17 @@ document.addEventListener('DOMContentLoaded', function () {
    * @method   getVersionAndYear
    * @return   {undefined}
    */
+
   function getVersionAndYear() {
 
     let
         manifest = chrome.runtime.getManifest(),
-        version = Array.from(document.getElementsByClassName('version')),
+        version = document.querySelector('.version'),
         year = new Date().getFullYear(),
         yearSpan = document.getElementById('year');
 
-    version.forEach(ver => { ver.textContent = 'version ' + manifest.version; });
+    version.textContent = 'version ' + manifest.version;
+
     yearSpan.textContent = year;
   }
 
@@ -41,9 +51,10 @@ document.addEventListener('DOMContentLoaded', function () {
    * @method   populateNavigation
    * @return   {undefined}
    */
+
   function populateNavigation() {
 
-    let features = Array.from(document.getElementsByClassName('feature'));
+    let features = [...document.getElementsByClassName('feature')];
 
     features.forEach(feature => {
 
@@ -51,27 +62,30 @@ document.addEventListener('DOMContentLoaded', function () {
 
       option.textContent = feature.textContent;
       option.value = feature.id;
+
       select.add(option);
     });
   }
+
 
   // ======================================================
   // UI Functionality
   // ======================================================
 
   // Scroll the page to the selected element
-  select.addEventListener('change', function() {
+  select.addEventListener('change', () => {
 
     location.hash = '#' + select.value;
 
-    if (location.hash.length !== 0) {
-      // (-50px to adjust for the navbar up top)
-      window.scrollTo(window.scrollX, window.scrollY - 80);
+    if ( location.hash.length !== 0 ) {
+      // (-80px to adjust for the navbar up top)
+      setTimeout(() => window.scrollTo(window.scrollX, window.scrollY - 80), 0);
     }
   });
 
+
   // ======================================================
-  // DOM Setup
+  // Init / DOM Setup
   // ======================================================
 
   getVersionAndYear();
