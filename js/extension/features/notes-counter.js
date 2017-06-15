0b6f8bfcf4945cb691c950dc5ce4971de39bf4d6
Matthew Salcido <matthew.salcido@healthsparq.com>
2017-06-15 07:39:30 -0700

    Minor refactor / documentation

--- js/extension/features/notes-counter.js
+++ js/extension/features/notes-counter.js
@@ -6,21 +6,34 @@
  * @website: http://www.msalcido.com
  * @github: https://github.com/salcido
  *
+ * ---------------------------------------------------------------------------
+ * Overview
+ * ---------------------------------------------------------------------------
+ *
+ * This feature will count the number of characters in the notes field.
+ *
+ * The script is initiated when the user clicks on any element with `.notes_show`
+ * or `.notes_text`.
+ *
+ * 1.) The click event is used to check for siblings with an existing note count. If
+ * found, it will be removed and a new one appended in its place.
+ * 2.) The count is updated with `keyup` events.
  */
 
 $(document).ready(function() {
 
-  $('body').on('click', '.notes_show, .notes_text', function(event) {
+  $('body').on('click', '.notes_show, .notes_text', function() {
 
     let count;
 
     // Wait for text field to be rendered in the DOM before
     // looking for its length value
-    setTimeout(function() {
+    setTimeout(() => {
 
       if ( $(':focus') ) {
 
         let notes = $(':focus').val(),
+            s = document.createElement('span'),
             siblings = $(':focus').siblings();
 
         // Look for existing count spans and remove them if necessary
@@ -35,10 +48,11 @@ $(document).ready(function() {
         count = notes ? notes.length : '0';
 
         // append the current character count from field
-        if ( !$(':focus').find('.de-notes-count') > -1 ) {
+        s.className = 'de-notes-count';
+        s.style = 'display:inline-block; padding:3px;';
+        s.textContent = `${count} / 255`;
 
-          $(':focus').parent().append(`<span class="de-notes-count" style="display:inline-block; padding:3px;">${count} / 255</span>`);
-        }
+        $(':focus').parent().append(s);
 
       } else {
 
@@ -58,6 +72,7 @@ $(document).ready(function() {
 
         parent.find('.de-notes-count').text(`${count} / 255`);
 
+        // warn when count total approaches limit
         if ( count > 240 ) {
 
           parent.find('.de-notes-count').addClass('price');
