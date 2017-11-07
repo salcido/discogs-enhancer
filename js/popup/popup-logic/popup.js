23ac2a329361839e6ddc9407917fafd1c7b3cde0
Matthew Salcido <matthew.salcido@healthsparq.com>
2017-11-07 13:42:33 -0800

    Fixed the gap in the Filter by Country expando

--- js/popup/popup-logic/popup.js
+++ js/popup/popup-logic/popup.js
@@ -127,7 +127,7 @@ window.addEventListener('load', function load() {
   // FILTER ITEMS BY COUNTRY OPTIONS
   // ========================================================
   document.querySelector('.toggle-group.country').addEventListener('click', function(event) {
-    optionsToggle('.hide-country', this, '.country', 135 );
+    optionsToggle('.hide-country', this, '.country', 115 );
   });
 
   // Save the Filter Items By Country CURRENCY select value to localStorage
