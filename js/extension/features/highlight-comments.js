4c0cda3fdf0a049f72b1b7f4b0d30aef37c046e8
Matthew Salcido <matthew.salcido@healthsparq.com>
2017-11-29 10:45:14 -0800

    friend comment highlighting

--- js/extension/features/highlight-comments.js
+++ js/extension/features/highlight-comments.js
@@ -6,6 +6,14 @@
  * @website: http://www.msalcido.com
  * @github: https://github.com/salcido
  *
+ * This will wait until the dashboard has loaded, then iterate over the
+ * friend's activity module and highlight any comments that have been
+ * made.
+ *
+ * Currently, this has to be activated using the options modal
+ * as it is an undocumented feature that seems only useful to myself.
+ * If anyone else is reading this and is curious, look up
+ * `dependencies/options/options.js` for more info :)
  */
 
 $(document).ready(function() {
@@ -13,23 +21,36 @@ $(document).ready(function() {
   let comments = resourceLibrary.options.highlightComments(),
       href = window.location.href;
 
+  // Check if we are on the dashboard
   if ( comments && href.includes('/my') ) {
 
+    // wait for the modules to load
     let int = setInterval(function() {
 
+      // Check if the friend module has been populated
       let friends = document.querySelectorAll('#dashboard_friendactivity tr').length;
 
       if ( friends ) {
 
+        // iterate over each comment
         let icons = document.querySelectorAll('.broadcast_table .icon-comment');
 
+        // add the class hook
         icons.forEach(i => {
 
           i.parentElement.parentElement.parentElement.classList.add('has-comments');
         });
 
+        // clear the interval
         clearInterval(int);
       }
     }, 13);
+
+    // Clear the interval regardless so the `setInterval` doesn't run forever
+    // in case there's no friend module on the dashboard
+    setTimeout(function () {
+
+      clearInterval(int);
+    }, 7000);
   }
 });
