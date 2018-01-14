e0141acfdbcdba9735269badbdde0592cf22a057
Matthew Salcido <salcido@users.noreply.github.com>
2018-01-14 14:28:45 -0800

    Hide Min, Median, Max columns feature

---
+++ js/extension/features/show-min-max-columns.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * Discogs Enhancer
+ *
+ * @author: Matthew Salcido
+ * @website: http://www.msalcido.com
+ * @github: https://github.com/salcido
+ *
+ */
+
+(function () {
+
+  let id = 'minMaxColumnsCss',
+      link = document.getElementById(id),
+      styles = link.getAttribute('disabled');
+
+  if (!styles) { link.setAttribute('disabled', true); }
+}());
