7b91e62696879575c53d30ec45dc35a8f07ef317
Matthew Salcido <matthew.salcido@healthsparq.com>
2018-01-17 13:32:10 -0800

    Use template strings in options

--- js/extension/dependencies/options/options.js
+++ js/extension/dependencies/options/options.js
@@ -13,39 +13,39 @@ $(document).ready(function() {
   let
       visible = false,
       loc = window.location.href,
-      modal = '<div id="optionsModal" class="options-modal" style="display: none;">' +
-                '<div class="options-modal-content">' +
-                  '<span class="options-close">x</span>' +
-                  '<h3>Super Secret Options Menu</h3>' +
-                  '<ul class="options">' +
-                    '<li>' +
-                      '<input id="analytics" name="analytics" type="checkbox" value="analytics" />' +
-                      '<label for="analytics">Analytics</label>' +
-                    '</li>' +
-                    '<li>' +
-                      '<input id="colorize" name="colorize" type="checkbox" value="colorize" />' +
-                      '<label for="colorize">Colorize Prices</label>' +
-                    '</li>' +
-                    '<li>' +
-                      '<input id="debug" name="debug" type="checkbox" value="debug" />' +
-                      '<label for="debug">Debug Messages</label>' +
-                    '</li>' +
-                    '<li>' +
-                      '<input id="comments" name="comments" type="checkbox" value="comments" />' +
-                      '<label for="comments">Highlight Comments</label>' +
-                    '</li>' +
-                    '<li>' +
-                      '<input id="unittests" name="unittests" type="checkbox" value="unittests" />' +
-                      '<label for="unittests">Unit Tests</label>' +
-                    '</li>' +
-                    '<li>' +
-                      '<label for="threshold" id="thresholdLabel">Threshold:</label>' +
-                      '<input id="threshold" name="threshold" type="number" value="" max="10" min="0"/>' +
-                    '</li>' +
-                  '</ul>' +
-                  '<a href="#" class="options-save button button_green" id="saveOptions">Save options &amp; refresh</a>' +
-                '</div>' +
-              '</div>';
+      modal = `<div id="optionsModal" class="options-modal" style="display: none;">
+                <div class="options-modal-content">
+                  <span class="options-close">x</span>
+                  <h3>Super Secret Options Menu</h3>
+                  <ul class="options">
+                    <li>
+                      <input id="analytics" name="analytics" type="checkbox" value="analytics" />
+                      <label for="analytics">Analytics</label>
+                    </li>
+                    <li>
+                      <input id="colorize" name="colorize" type="checkbox" value="colorize" />
+                      <label for="colorize">Colorize Prices</label>
+                    </li>
+                    <li>
+                      <input id="debug" name="debug" type="checkbox" value="debug" />
+                      <label for="debug">Debug Messages</label>
+                    </li>
+                    <li>
+                      <input id="comments" name="comments" type="checkbox" value="comments" />
+                      <label for="comments">Highlight Comments</label>
+                    </li>
+                    <li>
+                      <input id="unittests" name="unittests" type="checkbox" value="unittests" />
+                      <label for="unittests">Unit Tests</label>
+                    </li>
+                    <li>
+                      <label for="threshold" id="thresholdLabel">Threshold:</label>
+                      <input id="threshold" name="threshold" type="number" value="" max="10" min="0"/>
+                    </li>
+                  </ul>
+                  <a href="#" class="options-save button button_green" id="saveOptions">Save options &amp; refresh</a>
+                </div>
+              </div>`;
 
   // Keyboard Shortcut
   document.addEventListener('keyup', function(e) {
