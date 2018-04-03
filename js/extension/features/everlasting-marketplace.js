2addd20c71145dc3585d2096e9742e34de7a9e06
Matthew Salcido <salcido@users.noreply.github.com>
2018-04-02 20:39:04 -0700

    Refactoring continues ...

--- js/extension/features/everlasting-marketplace.js
+++ js/extension/features/everlasting-marketplace.js
@@ -17,13 +17,15 @@ resourceLibrary.ready(() => {
       pagination,
       paused = false;
 
+  // Make sure we are in the Marketplace
   if ( href.includes('/sell/mywants') || href.includes('/sell/list') ) {
 
     let
         blockList = JSON.parse(localStorage.getItem('blockList')) || null,
-        pTotal,
         filterUpdateLink,
-        language = resourceLibrary.language();
+        language = resourceLibrary.language(),
+        pTotal,
+        type = href.includes('/sell/mywants') ? 'mywants' : 'list';
 
     pagination = document.querySelector('.pagination_total').textContent;
 
@@ -103,92 +105,116 @@ resourceLibrary.ready(() => {
     });
 
     /**
+     * Injects the page result markup into the DOM
+     * @param {object} markup The releases markup that was fetched
+     * @returns {undefined}
+     */
+    function appendMarketplaceResults(markup) {
+
+      let lastChild = '#pjax_container tbody:last-child',
+          opt = document.createElement('option'),
+          selectBox = document.querySelector('.de-scroll-to-page'),
+          nextSetIndicator = `<tr class="shortcut_navigable">
+                                <td class="item_description">
+                                  <h2 class="de-current-page" id="de-page-${pageNum}">Page: ${pageNum}</h2>
+                                </td>
+                              </tr>`;
+
+      // Append page number to the DOM
+      document.querySelector(lastChild).insertAdjacentHTML('afterEnd', nextSetIndicator);
+      // Append new items to the DOM
+      document.querySelector(lastChild).insertAdjacentHTML('afterEnd', markup);
+
+      // Inject options into scroll-to-page select box
+      opt.value = pageNum;
+      opt.textContent = `Page: ${pageNum}`;
+      selectBox.insertAdjacentElement('beforeend', opt);
+    }
+
+    /**
+     * Calls any other Marketplace filtering features
+     * the user might have enabled.
+     * @method callOtherMarketplaceFeatures
+     * @returns {undefined}
+     */
+    function callOtherMarketplaceFeatures() {
+
+      // apply Marketplace Highlights
+      if (window.applyStyles) { window.applyStyles(); }
+
+      // apply price comparisons
+      if (window.injectPriceLinks) { window.injectPriceLinks(); }
+
+      // Hide/tag sellers in marketplace
+      if ( blockList && blockList.hide === 'global' && window.modifySellers ||
+            blockList && blockList.hide === 'marketplace' && window.modifySellers ) {
+
+        window.modifySellers('hide');
+      }
+
+      if ( blockList && blockList.hide === 'tag' && window.modifySellers ) {
+        window.modifySellers('tag');
+      }
+
+      // filter marketplace item by condition
+      if ( window.hideItems ) { window.hideItems(); }
+
+      // Filter marketplace by country
+      if ( window.filterByCountry ) { window.filterByCountry(); }
+
+      if ( window.sellersRep ) { window.sellersRep(); }
+
+      // Release ratings
+      if ( window.insertRatingsLink ) { window.insertRatingsLink(); }
+    }
+
+    /**
      * Grabs the next set of items
      * @method   getNextPage
      * @return   {undefined}
      */
     async function getNextPage() {
 
-      let type = href.includes('/sell/mywants') ? 'mywants' : 'list',
-          url = `/sell/${type}?page=${Number(pageNum)}${resourceLibrary.removePageParam(href)}`;
+      let url = `/sell/${type}?page=${Number(pageNum)}${resourceLibrary.removePageParam(href)}`;
 
       try {
 
-        let selectBox = document.querySelector('.de-scroll-to-page'),
-            response = await fetch(url, { credentials: 'include' }),
+        let response = await fetch(url, { credentials: 'include' }),
             data = await response.text(),
             div = document.createElement('div'),
-            opt = document.createElement('option'),
-            page = 'Page: ' + pageNum,
-            markup;
-        // TODO move all this into a method
+            markup,
+            noItems = '<h1 class="de-no-results">No more items for sale found</h1>',
+            tbody = '#pjax_container tbody';
+
         div.innerHTML = data;
 
-        markup = div.querySelector('#pjax_container tbody')
-                  ? div.querySelector('#pjax_container tbody').innerHTML
-                  : null;
+        markup = div.querySelector(tbody) ? div.querySelector(tbody).innerHTML : null;
 
         if ( markup ) {
 
-          let nextSetIndicator = `<tr class="shortcut_navigable">
-                                    <td class="item_description">
-                                      <h2 class="de-current-page" id="de-page-${pageNum}">${page}</h2>
-                                    </td>
-                                  </tr>`;
-
-          // Append page number to the DOM
-          document.querySelector('#pjax_container tbody:last-child').insertAdjacentHTML('afterEnd', nextSetIndicator);
-          // Append new items to the DOM
-          document.querySelector('#pjax_container tbody:last-child').insertAdjacentHTML('afterEnd', markup);
-
-          // Inject options into scroll-to-page select box
-          opt.value = pageNum;
-          opt.textContent = `Page: ${pageNum}`;
-          selectBox.insertAdjacentElement('beforeend', opt);
+          appendMarketplaceResults(markup);
 
         } else {
 
           document.querySelector('#de-next').remove();
-          document.querySelector('#pjax_container').insertAdjacentHTML('beforeend','<h1 class="de-no-results">No more items for sale found</h1>');
+          document.querySelector('#pjax_container').insertAdjacentHTML('beforeend', noItems);
         }
 
         pageNum++;
         hasLoaded = false;
 
-        // apply Marketplace Highlights
-        if (window.applyStyles) { window.applyStyles(); }
-
-        // apply price comparisons
-        if (window.injectPriceLinks) { window.injectPriceLinks(); }
-
-        // Hide/tag sellers in marketplace
-        if ( blockList && blockList.hide === 'global' && window.modifySellers ||
-             blockList && blockList.hide === 'marketplace' && window.modifySellers ) {
-
-          window.modifySellers('hide');
-        }
-
-        if ( blockList && blockList.hide === 'tag' && window.modifySellers ) {
-          window.modifySellers('tag');
-        }
-
-        // filter marketplace item by condition
-        if ( window.hideItems ) { window.hideItems(); }
-
-        // Filter marketplace by country
-        if ( window.filterByCountry ) { window.filterByCountry(); }
-
-        if ( window.sellersRep ) { window.sellersRep(); }
-
-        // Release ratings
-        if ( window.insertRatingsLink ) { window.insertRatingsLink(); }
+        callOtherMarketplaceFeatures();
 
       } catch (err) {
-        console.error('ahh snap', err);
+        console.error('Everlastning Marketplace could not fetch data', err);
       }
     }
 
-    // Resume loading shortcut
+    /**
+     * Adds the click event listner for `.de-resume`
+     * @method addResumeListener
+     * @returns {method}
+     */
     function addResumeListener() {
 
       document.querySelector('.de-resume').addEventListener('click', event => {
