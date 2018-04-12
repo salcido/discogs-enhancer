55bacb1501250974fd0820ef0a5e1f626d946249
Matthew Salcido <mattsalcido@gmail.com>
2018-04-12 08:17:01 -0700

    Simplify dark theme toggling

---
+++ js/extension/features/toggle-dark-theme.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * Discogs Enhancer
+ *
+ * @author: Matthew Salcido
+ * @website: http://www.msalcido.com
+ * @github: https://github.com/salcido
+ *
+ */
+
+(function() {
+
+  let id = 'darkThemeCss',
+      link = document.getElementById(id),
+      state = link.disabled ? false : true;
+
+  if (link) { link.disabled = state; }
+}());
