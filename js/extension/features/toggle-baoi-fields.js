25ed5a3f1371e1330a1ffa418eec40ff5611004d
Matthew Salcido <mattsalcido@gmail.com>
2018-04-12 08:41:38 -0700

    togglable filter by country

---
+++ js/extension/features/toggle-baoi-fields.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Discogs Enhancer
+ *
+ * @author: Matthew Salcido
+ * @website: http://www.msalcido.com
+ * @github: https://github.com/salcido
+ *
+ */
+
+(function () {
+
+    let link = document.getElementById('baoiFieldsCss'),
+        state = link.disabled;
+
+    if (link) { link.disabled = !state; }
+}());
