33ccdf7892ff35b4428427319f1de39d58504a87
Matthew Salcido <salcido@users.noreply.github.com>
2018-04-13 07:43:31 -0700

    Cleaning things up

--- js/extension/features/toggle-baoi-fields.js
+++ js/extension/features/toggle-baoi-fields.js
@@ -10,8 +10,7 @@
 
 (function () {
 
-  let link = document.getElementById('baoiFieldsCss'),
-      state = link.disabled;
+  let link = document.getElementById('baoiFieldsCss');
 
-  if (link) { link.disabled = !state; }
+  if ( link ) { link.disabled = !link.disabled; }
 }());
