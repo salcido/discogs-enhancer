5766531e83fdd76c68b60d4605d06cddfea76eee
Matthew Salcido <salcido@users.noreply.github.com>
2018-07-03 21:12:29 -0700

    Don't add seller-rep styles to favorite sellers

--- js/extension/features/seller-rep.js
+++ js/extension/features/seller-rep.js
@@ -58,6 +58,7 @@ resourceLibrary.ready(() => {
         if ( rating
              && rating < threshold
              && !seller_info[i].querySelector('.de-blocked-seller-icon')
+             && !seller_info[i].querySelector('.de-favorite-seller')
              && !seller_info[i].querySelector('.de-seller-rep-icon') ) {
 
           let icon = document.createElement('span');
