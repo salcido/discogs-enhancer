db0ab9e27cd6e400ee7c5296ce52bf5da4c1ecaa
Matthew Salcido <mattsalcido@gmail.com>
2018-07-09 15:50:09 -0700

    Get cookies method

--- js/extension/dependencies/resource-library/resource-library.js
+++ js/extension/dependencies/resource-library/resource-library.js
@@ -303,6 +303,35 @@
     },
 
     /**
+     * Gets the specified cookie
+     * @param cname {string} The name of the cookie to get
+     * @returns {string}
+     */
+
+    getCookie: function(cname) {
+
+    let name = cname + '=',
+        ca = document.cookie.split(';');
+
+      for (let i = 0; i < ca.length; i++) {
+
+        let c = ca[i];
+
+        while ( c.charAt(0) === ' ' ) {
+
+          c = c.substring(1);
+        }
+
+        if ( c.indexOf(name) === 0 ) {
+
+          return c.substring(name.length, c.length);
+        }
+      }
+
+      return '';
+    },
+
+    /**
      * Convenience method so I don't forget to parse
      * my localStorage objects.
      *
