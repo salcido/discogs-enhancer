d6c6448c6d0ee5447300a4063109ff8d8673ce7f
Matthew Salcido <salcido@users.noreply.github.com>
2020-02-03 17:24:47 -0800

    Add slowMo arg to test config

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ const path = require('path').join(__dirname, '../dist');
 const config = {
   headless: false,
   ignoreHTTPSErrors: true,
+  slowMo: 25,
   args: [
     `--disable-extensions-except=${path}`,
     `--load-extension=${path}`,
