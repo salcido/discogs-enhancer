b5ccbea45453618446b02a69f882eb203f0526dd
Matthew Salcido <salcido@users.noreply.github.com>
2020-04-08 07:20:48 -0700

    Bugfix - Allow Highlights to be togglable

--- js/extension/features/marketplace-highlights.js
+++ js/extension/features/marketplace-highlights.js
@@ -76,55 +76,12 @@ rl.ready(() => {
     addHighlights(orderSleeves);
   };
 
-  // ========================================================
-  // CSS
-  // ========================================================
-  let rules = `
-      .bold {
-        font-weight: bold !important;
-      }
-
-      .mint {
-          color: #00b4db !important;
-      }
-
-      .near-mint {
-        color: #00cfaa !important;
-      }
-
-      .very-good-plus {
-        color: #05cf21 !important;
-      }
-
-      .very-good {
-        color: #85ab11 !important;
-      }
-
-      .good-plus {
-        color: #f8b41f !important;
-      }
-
-      .good {
-        color: #d87307 !important;
-      }
-
-      .fair {
-        color: #e54803 !important;
-      }
-
-      .poor {
-        color: #ff0000 !important;
-      }
-    `;
-
 // ========================================================
 // DOM Setup
 // ========================================================
 
   // Apply styles on ready/prev/next clicks
   if ( rl.pageIs('allItems', 'sellRelease', 'myWants', 'seller', 'order') ) {
-
-    rl.attachCss('marketplace-highlights', rules);
     window.applyStyles();
     // Prev/Next clicks
     rl.handlePaginationClicks(window.applyStyles);
