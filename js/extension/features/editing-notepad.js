60b5f49fe9e6882711a426d6fd13c470bc3a4fd8
Ross Lancaster <toastymallows@gmail.com>
2020-11-28 20:22:07 -0500

    Add toggle button

--- js/extension/features/editing-notepad.js
+++ js/extension/features/editing-notepad.js
@@ -15,6 +15,19 @@ rl.ready( () => {
     return;
   }
 
+  // HTML variables
+  const className = 'de-editing-notepad';
+  const textAreaId = 'de-editing-notepad-textarea';
+  const toggleButtonId = 'de-editing-notepad-toggle-button';
+  const hiddenClass = 'de-editing-notepad-hidden';
+  const rotateClass = 'de-editing-notepad-rotate';
+  const showTitle = 'Show Notepad';
+  const hideTitle = 'Hide Notepad';
+
+  // Resource Library variables
+  const preferenceId = 'notepadText';
+  const visibilityPreferenceId = 'notepadVisible';
+
   function setNotepadText( text ) {
     rl.setPreference( preferenceId, text );
   }
@@ -23,17 +36,30 @@ rl.ready( () => {
     return rl.getPreference( preferenceId );
   }
 
-  const className = 'de-editing-notepad';
-  const id = 'de-editing-notepad-textarea';
-  const preferenceId = 'notepadText';
+  function setNotepadVisible( isVisible ) {
+    rl.setPreference( visibilityPreferenceId, isVisible );
+  }
+
+  function getNotepadVisible() {
+    return rl.getPreference( visibilityPreferenceId );
+  }
 
   if ( !getNotepadText() ) {
     setNotepadText( 'Notepad for storing information!' );
   }
 
+  const isNotepadInitiallyVisible = getNotepadVisible();
+
+  const initialTextAreaClasses = isNotepadInitiallyVisible ? '' : hiddenClass;
+  const initialToggleButtonClasses = isNotepadInitiallyVisible ? rotateClass : '';
+  const initialTitle = isNotepadInitiallyVisible ? hideTitle : showTitle;
+
   const markup = `
     <div class="${className}">
-      <div><textarea id="${id}">${getNotepadText()}</textarea></div>
+      <div>
+        <button id="${toggleButtonId}" class="${initialToggleButtonClasses}" title="${initialTitle}">◀</button>
+        <textarea id="${textAreaId}" class="${initialTextAreaClasses}">${getNotepadText()}</textarea>
+      </div>
     </div>
   `;
 
@@ -43,28 +69,62 @@ rl.ready( () => {
       right: 0;
       top: 50%;
       transform: translateY(-50%);
-      background-color: red !important;
-      height: 400px;
-      width: 300px;
+      height: 600px;
+      width: 400px;
     }
 
-    .${className} div {
+    .${className} > div {
       height: 100%;
       width: 100%;
-
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: flex-end;
+      align-items: center;
     }
 
-    .${className} textarea {
+    #${textAreaId} {
       height: 100%;
       width: 100%;
       margin; 5px;
+    }
+
+    #${textAreaId}.${hiddenClass} {
+      display: none;
+      visibility: hidden;
+    }
+
+    #${toggleButtonId} {
+      width: auto;
+      height: 1.1em;
+      font-size: xx-large;
+    }
+
+    #${toggleButtonId}.${rotateClass} {
+      transform: rotateY(180deg);
     }
   `;
 
   rl.attachCss( 'editing-notepad', css );
   document.body.insertAdjacentHTML( 'beforeend', markup );
 
-  document.getElementById( id ).addEventListener( 'input', ( event ) => {
+  const textArea = document.getElementById( textAreaId );
+  const toggleButton = document.getElementById( toggleButtonId );
+
+  textArea.addEventListener( 'input', ( event ) => {
     setNotepadText( event.target.value );
+  } );
+
+  toggleButton.addEventListener( 'click', ( event ) => {
+    if ( textArea.classList.contains( hiddenClass ) ) {
+      textArea.classList.remove( hiddenClass );
+      toggleButton.classList.add( rotateClass );
+      toggleButton.title = hideTitle;
+      setNotepadVisible( true );
+    } else {
+      textArea.classList.add( hiddenClass );
+      toggleButton.classList.remove( rotateClass );
+      toggleButton.title = showTitle;
+      setNotepadVisible( false );
+    }
   } );
 } );
