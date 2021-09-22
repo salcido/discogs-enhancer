36c6785997cf27d3c98ad4cbb66284364d009ffa
Matthew Salcido <salcido@users.noreply.github.com>
2021-09-21 20:05:38 -0700

    Fix uncleared setInterval issue

--- js/extension/features/show-actual-dates-react.js
+++ js/extension/features/show-actual-dates-react.js
@@ -11,110 +11,113 @@
 
  rl.ready(() => {
 
-  rl.waitForElement('div[class*="collection_"]').then(() => {
+  if ( rl.pageIs('release') ) {
 
-    let { usDateFormat } = rl.getItem('userPreferences'),
-        copies = document.querySelectorAll('div[class*="collection_"]'),
-        language = rl.language(),
-        monthList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+    rl.waitForElement('div[class*="collection_"]').then(() => {
 
-    // ========================================================
-    // Functions
-    // ========================================================
-    /**
-     * Sets a data attribute on each span that contains the
-     * relative date the item was added to the collection/wantlist
-     * @returns {Undefined}
-     */
-    function storeRelativeDates() {
-      let dates = document.querySelectorAll('div[class*="collection_"] span[class*="added_"]');
-      dates.forEach(date => { date.dataset.approx = date.textContent; });
-    }
+      let { usDateFormat } = rl.getItem('userPreferences'),
+          copies = document.querySelectorAll('div[class*="collection_"]'),
+          language = rl.language(),
+          monthList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
 
-    /**
-     * Converts time from 12 hour format to 24 hour format
-     * @param {String} time12h - The time in 12 hour format (1:15 PM)
-     * @returns {String}
-     */
-    function convertTo24(time12h) {
+      // ========================================================
+      // Functions
+      // ========================================================
+      /**
+       * Sets a data attribute on each span that contains the
+       * relative date the item was added to the collection/wantlist
+       * @returns {Undefined}
+       */
+      function storeRelativeDates() {
+        let dates = document.querySelectorAll('div[class*="collection_"] span[class*="added_"]');
+        dates.forEach(date => { date.dataset.approx = date.textContent; });
+      }
 
-      let [time, modifier] = time12h.split(' '),
-          [hours, minutes,] = time.split(':');
+      /**
+       * Converts time from 12 hour format to 24 hour format
+       * @param {String} time12h - The time in 12 hour format (1:15 PM)
+       * @returns {String}
+       */
+      function convertTo24(time12h) {
 
-      if ( hours === '12' ) { hours = '00'; }
+        let [time, modifier] = time12h.split(' '),
+            [hours, minutes,] = time.split(':');
 
-      if ( modifier === 'PM' ) { hours = parseInt(hours, 10) + 12; }
+        if ( hours === '12' ) { hours = '00'; }
 
-      return `${hours}:${minutes}`;
-    }
+        if ( modifier === 'PM' ) { hours = parseInt(hours, 10) + 12; }
 
-    /**
-    * Returns the localized month
-    * @param {Number} monthIndex - The index of the month from the `monthList` array
-    * @returns {String} - The localized month string
-    */
-    function getMonth(monthIndex) {
-      let objDate = new Date();
-      objDate.setMonth(monthIndex);
-      return objDate.toLocaleString(language, { month: 'long' });
-    }
+        return `${hours}:${minutes}`;
+      }
 
-    /**
-    * Renders the date format into the DOM
-    * @returns {undefined}
-    */
-    function renderDate(elem) {
-      // '5/4/2015, 11:41:51 PM'
-      let timestamp = elem.querySelector('span[class*="added_"]').title,
-          date = timestamp.split('/'),
-          timeRaw = date[2].split(' '),
-          year = timeRaw[0],
-          [hours, mins] = timeRaw[1].split(':'),
-          meridiem = timeRaw[2],
-          time = `${hours}:${mins} ${meridiem}`,
-          monthIndex = monthList.indexOf(date[0]),
-          international = `Added ${date[0]} ${getMonth(monthIndex)} ${year} ${convertTo24(time)}`,
-          american = `Added ${getMonth(monthIndex)} ${date[1]}, ${year} ${time}`,
-          specific = usDateFormat ? american : international;
+      /**
+      * Returns the localized month
+      * @param {Number} monthIndex - The index of the month from the `monthList` array
+      * @returns {String} - The localized month string
+      */
+      function getMonth(monthIndex) {
+        let objDate = new Date();
+        objDate.setMonth(monthIndex);
+        return objDate.toLocaleString(language, { month: 'long' });
+      }
 
-      elem.querySelector('span').textContent = specific;
-    }
+      /**
+      * Renders the date format into the DOM
+      * @returns {undefined}
+      */
+      function renderDate(elem) {
+        // '5/4/2015, 11:41:51 PM'
+        let timestamp = elem.querySelector('span[class*="added_"]').title,
+            date = timestamp.split('/'),
+            timeRaw = date[2].split(' '),
+            year = timeRaw[0],
+            [hours, mins] = timeRaw[1].split(':'),
+            meridiem = timeRaw[2],
+            time = `${hours}:${mins} ${meridiem}`,
+            monthIndex = monthList.indexOf(date[0]),
+            international = `Added ${date[0]} ${getMonth(monthIndex)} ${year} ${convertTo24(time)}`,
+            american = `Added ${getMonth(monthIndex)} ${date[1]}, ${year} ${time}`,
+            specific = usDateFormat ? american : international;
 
-    // ========================================================
-    // CSS
-    // ========================================================
-    let rules = `
-        div[class*="collection_"] span[class*="added_"] {
-          display: inline-block;
-          width: auto;
-          font-size: smaller;
-        }`;
+        elem.querySelector('span').textContent = specific;
+      }
 
-    // ========================================================
-    // DOM setup
-    // ========================================================
-    if ( usDateFormat === undefined ) usDateFormat = false;
+      // ========================================================
+      // CSS
+      // ========================================================
+      let rules = `
+          div[class*="collection_"] span[class*="added_"] {
+            display: inline-block;
+            width: auto;
+            font-size: smaller;
+          }`;
 
-    rl.attachCss('date-toggle', rules);
-    storeRelativeDates();
-    copies.forEach(copy => renderDate(copy));
+      // ========================================================
+      // DOM setup
+      // ========================================================
+      if ( usDateFormat === undefined ) usDateFormat = false;
 
-    // Event listeners
-    // ------------------------------------------------------
-    copies.forEach(copy => {
+      rl.attachCss('date-toggle', rules);
+      storeRelativeDates();
+      copies.forEach(copy => renderDate(copy));
 
-      let span = copy.querySelector('span[class*="added_"'),
-          actual = span.textContent;
+      // Event listeners
+      // ------------------------------------------------------
+      copies.forEach(copy => {
 
-      copy.querySelector('span[class*="added_"').addEventListener('mouseover', () => {
-        span.textContent = span.dataset.approx;
-      });
+        let span = copy.querySelector('span[class*="added_"'),
+            actual = span.textContent;
 
-      copy.querySelector('span[class*="added_"').addEventListener('mouseleave', () => {
-        span.textContent = actual;
+        copy.querySelector('span[class*="added_"').addEventListener('mouseover', () => {
+          span.textContent = span.dataset.approx;
+        });
+
+        copy.querySelector('span[class*="added_"').addEventListener('mouseleave', () => {
+          span.textContent = actual;
+        });
       });
     });
-  });
+  }
 });
 /*
 // ========================================================
