6a044001a87d5f58e9024b212a2cbd6155bda67e
Matthew Salcido <salcido@users.noreply.github.com>
2021-09-30 19:24:52 -0700

    Update force-dashboard.js

--- js/extension/features/force-dashboard.js
+++ js/extension/features/force-dashboard.js
@@ -9,15 +9,22 @@
  */
 
 rl.ready(() => {
-  try {
-    // normal release page
-    document.querySelector('#header_logo').href = '/my';
-  } catch (e) {
-    // react release page
+
+  if (rl.pageIsReact()) {
+
     let selector = 'a[class*="logo_"]';
 
     rl.waitForElement(selector).then(() => {
       document.querySelector(selector).href = '/my';
+      // let dashboardIcon = document.querySelector('div[class*="profile_"] a[href^="/my"]');
+      // dashboardIcon.style.display = 'none';
     });
+  } else {
+    // normal release page
+    document.querySelector('#header_logo').href = '/my';
+    // let dashboardIcons = document.querySelectorAll('.icon-dashboard');
+    // dashboardIcons.forEach(icon => {
+    //   icon.closest('li').style.display = 'none';
+    // });
   }
 });
