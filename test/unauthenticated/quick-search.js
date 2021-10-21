d247f0c6ea8dd9fa9c008c7e392e44c81f8075a0
Matthew Salcido <salcido@users.noreply.github.com>
2021-10-20 20:57:26 -0700

    Fix quick-search; rename class

--- test/unauthenticated/quick-search.js
+++ test/unauthenticated/quick-search.js
@@ -5,9 +5,9 @@ let test = async function(page) {
   await toggleFeature('#toggleQuickSearch');
   await Promise.all([
     page.goto('https://www.discogs.com/Lou-Karsh-Phantom-Structures/release/13705345', { waitUntil: 'networkidle2' }),
-    page.waitFor('.de-one-click')
+    page.waitFor('.de-quick-search')
   ]);
-  let hasQuickSearch = await page.$eval('.de-one-click', elem => elem.classList.contains('de-one-click'));
+  let hasQuickSearch = await page.$eval('.de-quick-search', elem => elem.classList.contains('de-quick-search'));
   assert.equal(hasQuickSearch, true, 'Quick Search span was not rendered');
 };
 
