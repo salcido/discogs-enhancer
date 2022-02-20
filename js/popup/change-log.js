3da6596663aea7d41110e2be27d7976370458ee2
Matthew Salcido <salcido@users.noreply.github.com>
2022-02-19 17:36:13 -0800

    Version bump to 2.25.22

--- js/popup/change-log.js
+++ js/popup/change-log.js
@@ -1,6 +1,20 @@
 module.exports = {
   current: [
     {
+      version: '2.25.22',
+      features: [
+      ],
+      updates: [
+        {
+          name: 'Bug Fix',
+          description: 'Fixed an issue with Inventory Scanner.'
+        }
+      ],
+      thanks: ['Huge thank you to Robert P. for their generous donation!']
+    },
+  ],
+  previous: [
+    {
       version: '2.25.21',
       features: [
       ],
@@ -12,8 +26,6 @@ module.exports = {
       ],
       thanks: ['Huge thank you to Robert P. for their generous donation!']
     },
-  ],
-  previous: [
     {
       version: '2.25.20',
       features: [
