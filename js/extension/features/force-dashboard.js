765167872061b93386db3984e170487a19d64156
Matthew Salcido <salcido@users.noreply.github.com>
2022-04-18 13:16:20 -0700

    Hide Dashboard icon in Nav

--- js/extension/features/force-dashboard.js
+++ js/extension/features/force-dashboard.js
@@ -10,21 +10,24 @@
 
 rl.ready(() => {
 
-  if (rl.pageIsReact()) {
+  if ( document.querySelector('#header_logo') ) {
+    // normal release page
+    document.querySelector('#header_logo').href = '/my';
+    // Hide Dashboard Icon
+    let dashboardIcons = document.querySelectorAll('.icon-dashboard');
+    dashboardIcons.forEach(icon => {
+      icon.closest('li').style.display = 'none';
+    });
 
+  } else {
+    // React release page
     let selector = 'a[class*="logo_"]';
 
     rl.waitForElement(selector).then(() => {
       document.querySelector(selector).href = '/my';
-      // let dashboardIcon = document.querySelector('div[class*="profile_"] a[href^="/my"]');
-      // dashboardIcon.style.display = 'none';
+      // Hide Dashboard Icon
+      let dashboardIcon = document.querySelector('div[class*="profile_"] a[href^="/my"]');
+      dashboardIcon.style.display = 'none';
     });
-  } else {
-    // normal release page
-    document.querySelector('#header_logo').href = '/my';
-    // let dashboardIcons = document.querySelectorAll('.icon-dashboard');
-    // dashboardIcons.forEach(icon => {
-    //   icon.closest('li').style.display = 'none';
-    // });
   }
 });
