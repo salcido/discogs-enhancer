ea3d446895be3cdee828c58ccd29a222ce38acd6
Matthew Salcido <salcido@users.noreply.github.com>
2022-04-29 15:11:30 -0700

    Get values from featurePrefs

--- js/extension/features/filter-monitor.js
+++ js/extension/features/filter-monitor.js
@@ -13,11 +13,11 @@
 rl.ready(() => {
 
   let currentFilterState = rl.getPreference('currentFilterState'),
-      sleeveCondition = rl.getPreference('sleeveCondition') || { value: null, generic: false, noCover: false },
-      filterPrices = rl.getPreference('filterPrices') || { minimum: null, maximum: null },
+      { sleeveCondition } = rl.getPreference('featurePrefs'),
+      { filterPrices } = rl.getPreference('featurePrefs'),
       userCurrency = rl.getPreference('userCurrency'),
-      countryList = rl.getPreference('countryList') || { list: [], currency: false, include: false },
-      mediaCondition = rl.getPreference('mediaCondition') || null;
+      { countryList } = rl.getPreference('featurePrefs'),
+      { mediaCondition } = rl.getPreference('featurePrefs');
 
   let countryEnabled = currentFilterState.filterShippingCountry,
       currency = countryList && countryList.currency ? countryList.currency : null,
