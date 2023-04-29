b4819dbf076194354f98e617a3384404fafc807c
Matthew Salcido <salcido@users.noreply.github.com>
2023-04-29 07:46:49 -0700

    Fix tooltip issues

--- js/extension/features/random-item.js
+++ js/extension/features/random-item.js
@@ -35,20 +35,38 @@
       .de-random-item {
         cursor: pointer;
         font-size: 14px;
-        margin-right: 10px;
+        padding: 0 30px !important;
         color: white;
+      }
+
+      .de-random-item:hover {
+        background: black;
+      }
+
+      .de-random-item .tooltip {
+        opacity: 0;
+      }
+
+      .de-random-item:hover .tooltip {
+        opacity: 1;
       }
 
       .de-random-item svg {
         position: absolute;
         top: 22px;
         left: -10px;
-        fill: ${document.querySelector('.de-dark-theme') ? '#cccccc' : '#ffffff'};
+        fill: #ffffff;
       }
 
       .de-random-item span,
       .de-random-item span i {
         pointer-events: none;
+      }
+
+      .de-random-item .tooltip {
+        position: absolute;
+        top: 63px;
+        left: -17px;
       }
 
       .rotate-in {
@@ -84,7 +102,7 @@
   // ========================================================
   let user = rl.username(),
       icon = `<li style="position: relative;">
-                <a class="nav_group_control de-random-item needs_delegated_tooltip rotate-in"
+                <a class="nav_group_control de-random-item has-tooltip rotate-in"
                    href="/user/${user}/collection/random"
                    rel="tooltip"
                    data-placement="bottom"
@@ -92,6 +110,11 @@
                   <span style="position: relative">
                    <svg width="20" height="17" viewBox="0 0 15 12" xmlns="http://www.w3.org/2000/svg"><path d="M14.9998 9.45703L11.85 12V10.3044H11.3198C9.64588 10.3044 8.41328 9.59801 7.39572 8.66267C7.78817 8.22752 8.14397 7.78952 8.47497 7.37975C8.55101 7.28523 8.62656 7.19195 8.70195 7.09867C9.45476 7.83191 10.2645 8.32656 11.32 8.32656H11.8502V6.9135L14.9998 9.45703V9.45703ZM0 3.80524H1.56967C2.56507 3.80524 3.34176 4.24592 4.05831 4.90998C4.17623 4.76739 4.29547 4.61885 4.41583 4.47054C4.70881 4.1072 5.01853 3.72463 5.35412 3.34308C4.36327 2.47094 3.16781 1.82692 1.56971 1.82692H0V3.80524V3.80524ZM11.85 0V1.68576H11.3198C8.54949 1.68576 6.9876 3.61988 5.60929 5.32689C4.36954 6.8596 3.29919 8.1851 1.56967 8.1851H0V10.1632H1.56967C4.34043 10.1632 5.90228 8.23008 7.28063 6.52348C8.51994 4.98893 9.59049 3.66362 11.3198 3.66362H11.85V5.08692L15 2.54316L11.85 0V0Z"/></svg>
                   </span>
+                  <span class="tooltip" role="tooltip">
+                    <span class="tooltip-inner">
+                      Random Item
+                    </span>
+                </span>
                 </a>
               </li>`;
 
@@ -103,6 +126,7 @@
 
     document.querySelector('.de-random-item').addEventListener('click', event => {
       event.target.classList.replace('rotate-in', 'rotate-out');
+      event.target.querySelector('.tooltip').style.opacity = 0;
       if (event.metaKey) {
         return setTimeout(() => stopAnimation(), 250);
       }
