b2c41792d4cd422cf1339ea05f2f1916276ca576
Matthew Salcido <salcido@users.noreply.github.com>
2023-05-04 08:24:42 -0700

    Update change-log.js

--- js/popup/change-log.js
+++ js/popup/change-log.js
@@ -1,6 +1,37 @@
 module.exports = {
   current: [
     {
+      version: '3.2.3',
+      features: [
+        {
+          name: 'More Dark Themes',
+          description: 'Three dark themes are now available! Meteor, Gravity, and Nebula. Check them out and let me know what you think! You can also set the Dark Theme to sync with your operating system.',
+          link: '#darkTheme'
+        },
+      ],
+      updates: [
+        {
+          name: 'Enhancement',
+          description: 'You can now sync the dark theme to your operating system\'s preference by clicking the Sync checkbox when the dark theme is enabled!'
+        },
+        {
+          name: 'Enhancement',
+          description: 'eBay (US) and Decks.de have been added to the Contextual Menu options.'
+        },
+        {
+          name: 'Bug Fix',
+          description: 'Fixed an issue where the "Has x more items I want" links in the Marketplace might not show up when using the Filter By Country feature.'
+        },
+        {
+          name: 'Bug Fix',
+          description: 'Fixed an issue where the Random Item tool tip would not be shown.'
+        },
+      ],
+      thanks: ['Huge thank you to Joe McCormick and the anonymous donor for the donations!'],
+    },
+  ],
+  previous: [
+    {
       version: '3.2.2',
       features: [
         {
@@ -25,8 +56,6 @@ module.exports = {
       ],
       thanks: ['Huge thank you to Joe McCormick and the anonymous donor for the donations!'],
     },
-  ],
-  previous: [
     {
       version: '3.2.1',
       features: [
