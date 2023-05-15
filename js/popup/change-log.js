3a3d0f75b2d892fca99c09e2021e16b46b80cb7e
Matthew Salcido <salcido@users.noreply.github.com>
2023-05-15 15:55:23 -0700

    Update change-log.js

--- js/popup/change-log.js
+++ js/popup/change-log.js
@@ -1,30 +1,22 @@
 module.exports = {
   current: [
     {
-      version: '3.2.3',
+      version: '3.3.0',
       features: [
         {
-          name: 'More Dark Themes',
-          description: 'Three dark themes are now available! Meteor, Gravity, and Nebula. Check them out and let me know what you think! You can also set the Dark Theme to sync with your operating system.',
-          link: '#darkTheme'
+          name: 'Shopping Spree Mode 🎉',
+          description: 'When Shopping Spree Mode is enabled you can add items to your cart without leaving the Marketplace. Go wild!',
+          link: '#shoppingSpree'
         },
       ],
       updates: [
         {
           name: 'Enhancement',
-          description: 'You can now sync the dark theme to your operating system\'s preference by clicking the Sync checkbox when the dark theme is enabled!'
-        },
-        {
-          name: 'Enhancement',
           description: 'eBay (US) and Decks.de have been added to the Contextual Menu options.'
         },
         {
-          name: 'Bug Fix',
-          description: 'Fixed an issue where the "Has x more items I want" links in the Marketplace might not show up when using the Filter By Country feature.'
-        },
-        {
-          name: 'Bug Fix',
-          description: 'Fixed an issue where the Random Item tool tip would not be shown.'
+          name: 'Enhancement',
+          description: 'You can now sync the dark theme to your operating system\'s preference by clicking the Sync checkbox when the dark theme is enabled!'
         },
       ],
       thanks: ['Huge thank you to Joe McCormick and the anonymous donor for the donations!'],
