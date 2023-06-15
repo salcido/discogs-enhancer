455f3dbdc925ef170582b493dc7c45adf1e6617d
Matthew Salcido <salcido@users.noreply.github.com>
2023-06-14 19:05:32 -0700

    Update random-item-react.js

--- js/extension/features/random-item-react.js
+++ js/extension/features/random-item-react.js
@@ -14,8 +14,8 @@
  */
 
  rl.ready(() => {
-
-  if (!rl.pageIsReact()) return;
+  let newHeader = document.querySelector('div[id*="__header"]');
+  if (!rl.pageIsReact() || newHeader) return;
   // ========================================================
   // Function
   // ========================================================
