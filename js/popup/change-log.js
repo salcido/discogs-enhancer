1f3489dbe363cbb02e312012a6a8ced3a014f826
Matthew Salcido <salcido@users.noreply.github.com>
2023-09-19 17:51:52 -0700

    Update donors

--- js/popup/change-log.js
+++ js/popup/change-log.js
@@ -13,7 +13,7 @@ module.exports = {
           description: 'The Tag Seller Rating feature now lets you hide new sellers in the Marketplace. Expand the feature menu and click the Hide New Sellers checkbox to enable it.'
         },
       ],
-      thanks: ['Massive thank you to Archibald, Arturo, Britney, Charlie, Chris, Daniel, Douglas, Fabian, Frederic, Henrik, James, Jeffrey, Jesper, Johan, Johannes, Jonathan, Jordan, Lorenzo, Loris, Lukas, Łukasz, Mania, Marc, Marcel, Mark, Mike, Nathan, Nicholas, Niko\'s Records, Noah, Pangiotis, Paul, Paull, Peter, Robert, Ryan, Simon, Socrates, Stewart William, and Willem for the kind donations! I am overwhelmed by your generosity. 🥲'],
+      thanks: ['Massive thank you to Lewis B. and Britany for the kind donations!'],
     },
   ],
   previous: []
