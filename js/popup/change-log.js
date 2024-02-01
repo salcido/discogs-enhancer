93621b82707d9181c4d7eb8bde255035d6b539d1
Matthew Salcido <salcido@users.noreply.github.com>
2024-01-31 19:12:57 -0800

    Update change-log.js

--- js/popup/change-log.js
+++ js/popup/change-log.js
@@ -3,14 +3,18 @@ module.exports = {
     {
       features: [{
         name: 'Collection Box Fix',
-        description: 'Threw together a quick feature that moves the "In Collection" box back to it\'s original location on the Release Page.',
+        description: 'Threw together a quick feature that moves the "In Collection" and "In Wantlist" boxes back to their original locations on the Release Page.',
         link: '#collectionBoxFix',
       }],
       removedFeatures: [],
       updates: [
         {
-          name: 'Enhancement',
-          description: 'Release Scanner has been updated to work with the new Artist Page. Let me know if you find any issues with it.'
+          name: 'Update',
+          description: 'Dashboard Link has been renamed to Original Dashboard Link'
+        },
+        {
+          name: 'Update',
+          description: 'Release Scanner has been updated to work with the new Artist and Label Pages. Let me know if you find any issues with it.'
         },
       ],
       thanks: ['Massive thank you to Thomas L. and Noah S. for the kind donations!'],
