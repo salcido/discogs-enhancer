0cda04ee6dea3f67fbe42b4277a9b33521dd4705
Matthew Salcido <salcido@users.noreply.github.com>
2025-04-20 13:36:25 -0700

    Update change-log.js

--- js/popup/change-log.js
+++ js/popup/change-log.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       features: [{
         name: 'Umbra Dark Theme',
-        description: 'Introducing "Umbra", an even darker Dark Theme! In keeping with the celestial theme, "Umbra" means: The completely dark central portion of the shadow cast by the earth, moon, or other body during an eclipse.',
+        description: 'Introducing the new "Umbra" Dark Theme! In keeping with the celestial names, "Umbra" means: The completely dark central portion of the shadow cast by the earth, moon, or other body during an eclipse.',
         link: '#darkTheme'
       }],
       removedFeatures: [],
