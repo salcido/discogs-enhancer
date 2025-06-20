1634348b40cfdc31c5a2563cca2c7d9e4d718113
Matthew Salcido <salcido@users.noreply.github.com>
2025-06-20 14:04:40 -0700

    Update donors list

--- js/popup/change-log.js
+++ js/popup/change-log.js
@@ -10,10 +10,10 @@ module.exports = {
       updates: [
         {
           name: 'Enhancement',
-          description: 'I\'ve added a new badge alert to the extension\'s icon in the browser that will show up next time there is a change on Discogs that breaks something with Discogs Enhancer. The aim is to show you that I am aware of the issues and working on a fix. Not sure if this will be annoying or not so please let me know if you hate it.'
+          description: 'More Dark Theme updates to fix the coloring issues in the Nav bar.'
         },
       ],
-      thanks: [],
+      thanks: ['Massive thank you to everyone who donated this year! I really appreacite it :)'],
     },
   ],
   previous: []
